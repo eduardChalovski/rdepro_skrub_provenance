f258a73a3e9c012daefe918891dd340b39651be2
tAleksiev2003 <teodoraleksiev2003@gmail.com>
2025-12-02 11:20:06 +0100

    pd.merge complete. Added comments

--- mystuff/monkeypatch.py
+++ mystuff/monkeypatch.py
@@ -69,14 +69,13 @@ _original_merge_top = pd.merge
 @wraps(_original_merge_top)
 def provMerge(left: pd.DataFrame, right: pd.DataFrame, *args, **kwargs):
     EddieProvenanceReporter(_original_merge_top, kwargs, args)
-    left = with_provenance(left, "left") 
-    right = with_provenance(right, "right")
-    left = left.rename(columns={PROV_COLUMN: "_prov_left"})
-    right = right.rename(columns={PROV_COLUMN: "_prov_right"})
+    left = with_provenance(left, "left") # a check should be done if there are already provenance collumns, THIS IS NOT FUTURE PROOF
+    right = with_provenance(right, "right")# A different apporach is wrapping the df class and running this funciton on creation of DF
+    left = left.rename(columns={PROV_COLUMN: "_prov_left"}) # i did not do this as i did not figure out a sexy way to name the collumn
+    right = right.rename(columns={PROV_COLUMN: "_prov_right"}) #in a generic way, where its called "prov_[df name]. That approach is deffinetly future proof and does not break"
     #will do with a switch later
     how = kwargs.get("how")
     if (how == "outer"): #OUTER JOIN IS AN UNION
-
         output = left.merge(right, on= kwargs.get("on"), how = "outer")
         provList = []
         for i in range(len(output["_prov_left"])):
@@ -85,9 +84,12 @@ def provMerge(left: pd.DataFrame, right: pd.DataFrame, *args, **kwargs):
         output = output.drop(columns=["_prov_left", "_prov_right"])
 
         return output
-    elif (how == "inner"): #INNER JOIN IS AN INTERSECTION
-
-        output = left.merge(right, on= kwargs.get("on"), how = "inner")
+    elif (how == "inner" or how == "left" or how == "right"): #INNER JOIN IS AN INTERSECTION
+        # An inner join can be done as a cartesian product x selection for the provenance tuples
+        # however is simpler and more logical if we just apply the rules for an intersection
+        # also from my understanding, the inner join is a more concise case of left and right join
+        # so the logic here for the polynomials should be the same
+        output = left.merge(right, on= kwargs.get("on"), how = how)
         provList = []
         for i in range(len(output["_prov_left"])):
             provList.append(f"{_product_provenances(output['_prov_left'][i], output["_prov_right"][i])}")
@@ -102,7 +104,5 @@ pd.merge = provMerge
 
 # Patch method on DataFrame
 
-#@wraps(pd.DataFrame.merge)
-#def leftRightJoin(*args, **kwags):
 
 
